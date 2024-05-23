d141a1f689ee73be35ce743051b4c1a4b70d5427
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-05-23 09:22:00 +0200

    add comments

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <?php
 
+// collego il file function.php a questo file 
 require "partials/function.php";
-// creo variabile per salvare il valore dell'input
+
+// creo variabile per salvare il valore dell'input mediante l'attributo name "comevoglio"
+// POST è una superglobale di PHP che permette di accedere ai valori inseriti nei campi del form dopo che l'utente ha cliccato su "Submit"
 $salva_valore = $_POST['comevoglio'];
 
-
+// il risultato sarà una variabile speculare, in cuiviene però passata la funzione controlla_simboli(che controlla la presenza dei simboli . e @)
+// richiamando questa variabile sulla sezione html per la validazione, con l'aiuto di un operatore ternario riesco a mostrare messaggi pertinenti. vedi riga 82
 $result = controlla_simboli($salva_valore);
 
+// stessa cosa. applico la funzione valore_vuoto a salva_valore(quindi al valore inserito nell'input dall'utente) e poi la richiamo a riga 71 per decretare, grazie a operatore ternario, il messaggio pertinente da mostrare
+$value_result = valore_vuoto($salva_valore);
+
 var_dump($result);
+
 
 ?>
 
@@ -31,12 +39,18 @@ var_dump($result);
             <div class="col">
                 
                 <form action="index.php" method="POST">
-                  <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="comevoglio">
-                  </div>
-                  
-                  <button type="submit" class="btn btn-primary">Submit</button>
+
+                    <div class="mb-3">
+
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+
+                        <!-- sull'input specifico l'attributo name e lo collego alla variabile salva_valore applicandovi il metodo _POST -->
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="comevoglio">
+
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary">Submit</button>
+
                 </form>
 
             </div>
@@ -44,9 +58,23 @@ var_dump($result);
 
         </div>
 
+
+        <!-- sezione validazione con layout bootstrap -->
         <div class="row">
 
             <div class="col">
+
+                <div>
+
+                    <?php 
+
+                    echo $value_result ?
+                            '<h2> Inserisci Mail </h2>' :
+                            ''
+
+                    ?>
+
+                </div>
 
                 <div>
                     <?php  
--- partials/function.php
+++ partials/function.php
@@ -1,7 +1,14 @@
 <?php
 
 function controlla_simboli ($mail_value) {
-    
+
+    // str_contains verifica la presenza di una sottostringa all'interno della stringa
     return str_contains($mail_value, ".") && str_contains($mail_value, "@");
 
+}
+
+function valore_vuoto ($mail_value) {
+
+    // empty verifica se una variabile è vuota
+    return empty($mail_value);
 }
